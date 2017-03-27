dffeb0f555e148c338f6524d8573bb88239640f5
ziplistNightlife <magazinemountain1@gmail.com>
2017-03-26 22:51:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'zipnightlife!@#$%^&*',
+  appId: process.env.APP_ID || 'zipnightlife123',
   masterKey: process.env.MASTER_KEY || 'zipnightlife', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://ziplist.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
